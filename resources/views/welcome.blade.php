5de740fcd4e63b44125da1c069d111771171738f
Maxwell Oliveira <usuarioamericaservis@gmail.com>
2020-11-04 16:15:53 -0300

    Mudanças na logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <img src="images/LogoCompleta.png" class="img-fluid" alt="Imagem responsiva" height="200%">                   
+                    <img src="images/LogoCompleta.png" class="img-fluid" alt="Imagem responsiva" height="5px">                   
                 </div>
 
             </div>
