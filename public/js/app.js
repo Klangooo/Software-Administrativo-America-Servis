83c0c710936a732a81bdd6612e20208c563b76b4
LuigiNegrini <luigi.99999@hotmail.com>
2020-11-13 11:46:47 -0300

    model para o bd

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! H:\Aplicações\Laravel\Ponto\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! H:\Aplicações\Laravel\Ponto\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\ponto\whispering-gorge-97868\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\ponto\whispering-gorge-97868\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
