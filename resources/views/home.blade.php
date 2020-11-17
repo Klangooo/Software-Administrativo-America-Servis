98f16d699cb0a9a19e205fb4eb4cd2e3c3b70503
Maxwell Oliveira <usuarioamericaservis@gmail.com>
2020-11-16 22:47:39 -0300

    cores na home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,12 @@
 @extends('layouts.template_base')
 
 @section('content')
-
-<style>
-    .primeiralinha {
-      background-color:#032066;
-      color: white;
-      font-family: Verdana, Geneva, Tahoma, sans-serif;
-    }
-</style>
-
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="primeiralinha">
             <div class="card">
-                <div class="card-header">{{ __('Painel de Controle') }}</div>
+                <div class="p-3 mb-2 bg-primary text-white">
+                    <div class="card-header">{{ __('Painel de Controle') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
