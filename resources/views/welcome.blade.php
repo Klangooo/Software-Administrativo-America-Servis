a3465b3995b160d26df3290536921411dfc8bf77
VitoriaBueno <vitoria-bguedes@live.com>
2020-11-22 11:56:27 -0300

    et 5.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,13 +65,13 @@
             .cabecalho {
                 background: #0e72be;
             }
-            
+
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height cabecalho">
+        <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div class="top-right links cabecalho">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
