1b128646c1ca4b0a82e96f0365b8d1b69b06c496
LuigiNegrini <luigi.99999@hotmail.com>
2020-11-26 17:06:07 -0300

    botao pagina registrar

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -80,7 +80,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-secondary rounded-pill botao" style="background-color: #032066;color: white;float: right;border-color: white;">
+                                <button type="submit" class="btn btn-secondary rounded-pill botao" >
                                     {{ __('Criar usuário') }}
                                 </button>
                             </div>
