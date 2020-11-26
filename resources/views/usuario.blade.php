a09fa78fd84b016239f97be15d28f8417b67e8de
LuigiNegrini <luigi.99999@hotmail.com>
2020-11-26 14:42:49 -0300

    register

--- resources/views/usuario.blade.php
+++ resources/views/usuario.blade.php
@@ -154,7 +154,7 @@ use App\User;
           @csrf
 
           <div class="form-group row">
-              <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+              <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
               <div class="col-md-6">
                   <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -168,7 +168,7 @@ use App\User;
           </div>
 
           <div class="form-group row">
-              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
               <div class="col-md-6">
                   <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -182,7 +182,7 @@ use App\User;
           </div>
 
           <div class="form-group row">
-              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
 
               <div class="col-md-6">
                   <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -196,7 +196,7 @@ use App\User;
           </div>
 
           <div class="form-group row">
-              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirme a senha') }}</label>
 
               <div class="col-md-6">
                   <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -206,7 +206,7 @@ use App\User;
           <div class="form-group row mb-0">
               <div class="col-md-6 offset-md-4">
                   <button type="submit" class="btn btn-primary">
-                      {{ __('Register') }}
+                      {{ __('Criar usuário') }}
                   </button>
               </div>
           </div>
