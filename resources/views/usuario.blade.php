0f9ff1805c145a2dda84b0749e9328ff43a3a242
Maxwell Oliveira <usuarioamericaservis@gmail.com>
2020-11-26 08:28:16 -0300

    Espaçamento dos modais

--- resources/views/usuario.blade.php
+++ resources/views/usuario.blade.php
@@ -195,9 +195,7 @@ use App\User;
           <span aria-hidden="true">&times;</span>
           </button>
       </div>
-      .ml-1 {
-        margin-left: ($spacer * .25) !important;
-      }
+      <div class="mx-auto" style="width: 100px;">
       <?php
        // $usuario->id = '1';
       ?>
@@ -210,8 +208,8 @@ use App\User;
         <div class="form-group">
             {{Form::label('email', 'Email')}}
             {{Form::text('email', $usuario->email, ['class' => 'form-control', 'placeholder' => 'Email'])}}
-        
-      </div>
+        </div>
+        </div>
         <div class="modal-footer">
         <button type="button" class="btn btn-secondary rounded-pill botao" data-dismiss="modal">Cancelar</button>
         {{Form::hidden('_method', 'PUT')}}
