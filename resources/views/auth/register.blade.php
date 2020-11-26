d2e6a0787c54f12a7a1cf8a0638fa120cb2781b8
LuigiNegrini <luigi.99999@hotmail.com>
2020-11-26 17:10:05 -0300

    cabeçalho app

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,6 @@
   .botao {
     background-color: #032066;
     color: white;
-    float: right;
     border-color: white;
   }
 
@@ -80,7 +79,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-secondary rounded-pill botao" >
+                                <button type="submit" class="btn btn-secondary rounded-pill botao">
                                     {{ __('Criar usuário') }}
                                 </button>
                             </div>
