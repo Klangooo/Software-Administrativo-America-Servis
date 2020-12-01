73ebb1431eff3212c3a8468a77cef223012ff728
LuigiNegrini <luigi.99999@hotmail.com>
2020-12-01 12:44:52 -0300

    botoes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,7 +64,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-secondary rounded-pill" style="background-color: #032066;color: white;float: right;border-color: white;">
+                                <button type="submit" class="btn btn-secondary rounded-pill" style="background-color: #032066;color: white;border-color: white;">
                                     {{ __('Criar usuário') }}
                                 </button>
                             </div>
