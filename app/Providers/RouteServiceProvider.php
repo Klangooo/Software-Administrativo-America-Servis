43a0acc0dbcd7f1e9b87d2b5993a872e2a5ffb7f
LuigiNegrini <luigi.99999@hotmail.com>
2020-12-03 00:05:41 -0300

    direcionamento usuario-register

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const FUNC = '/funcionarios';
+
 
     /**
      * Define your route model bindings, pattern filters, etc.
