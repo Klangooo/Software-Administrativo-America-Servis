0eae3ebbead265ef67259409c1b01c37d8bcfde3
Vitoria <vitoria-bguedes@live.com>
2020-12-10 11:06:40 -0300

    pag ponto, print nome

--- app/Http/Controllers/PontosController.php
+++ app/Http/Controllers/PontosController.php
@@ -49,7 +49,7 @@ class PontosController extends Controller
         {
             // Create Post
             $ponto = new Ponto;
-            $ponto->nome = DB::table('funcionarios')->where('cpf', $cpf)->get('nome');
+            $ponto->nome = DB::table('funcionarios')->where('cpf', $cpf)->get('nome')->nome;
             $ponto->cargo = DB::table('funcionarios')->where('cpf', $cpf)->get('cargo');
             $ponto->postodeservico = DB::table('funcionarios')->where('cpf', $cpf)->get('postodeservico');
 
