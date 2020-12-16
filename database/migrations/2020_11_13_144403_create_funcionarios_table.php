7300c589395303fba1adb88500880c28ba0749b1
LuigiNegrini <luigi.99999@hotmail.com>
2020-12-16 14:42:33 -0300

    latitude e longitude dos funcionarios

--- database/migrations/2020_11_13_144403_create_funcionarios_table.php
+++ database/migrations/2020_11_13_144403_create_funcionarios_table.php
@@ -19,8 +19,8 @@ class CreateFuncionariosTable extends Migration
             $table->string('cpf')->unique();
             $table->string('cargo');
             $table->string('postodeservico');
-            $table->floatval('latitude');
-            $table->floatval('longitude');
+            $table->float('latitude');
+            $table->float('longitude');
             $table->timestamps();
         });
     }
