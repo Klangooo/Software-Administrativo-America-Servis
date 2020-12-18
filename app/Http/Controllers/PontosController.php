2c7454300fb646f393b7a222ee98fbe66e9606c7
LuigiNegrini <luigi.99999@hotmail.com>
2020-12-17 21:52:27 -0300

    testes bd data

--- app/Http/Controllers/PontosController.php
+++ app/Http/Controllers/PontosController.php
@@ -70,11 +70,11 @@ class PontosController extends Controller
             $ponto->iniciointervalo = $request->input('iniciointervalo');
             $ponto->fimintervalo = $request->input('fimintervalo');
             $ponto->saida = $request->input('saida');
-            
+
             // DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
             date_default_timezone_set('America/Sao_Paulo');
             // CRIA UMA VARIAVEL E ARMAZENA A HORA ATUAL DO FUSO-HORÀRIO DEFINIDO (BRASÍLIA)
-            $ponto->data = date('d/m/Y H:i:s');
+            $ponto->data = date('d/m/Y');
 
             $ponto->save();
             return redirect('/ponto');
