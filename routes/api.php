2c2892bbedc4b2010cadbcac6102889c476d9a56
LuigiNegrini <luigi.99999@hotmail.com>
2020-12-22 22:39:12 -0300

    conexao app - softw

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/entrada', function (Request $request) {
+Route::post('/entrada', function (Request $request) {
     return $request;
 });
 
